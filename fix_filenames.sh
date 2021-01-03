77fbed679af346fe3ee43a37005a2436fd680c35
Jérémie <jer.despraz@gmail.com>
2021-01-03 15:35:15 +0100

    Create fix_filenames.sh

---
+++ fix_filenames.sh
@@ -0,0 +1,35 @@
+# convert encodings if needed (e.g. if filenames or folders contain special characters)
+  convmv -f utf-8 -t utf-8 --notest --nfc --replace -r <target_folder>
+
+# Use the detox debian package to cleanup problematic filenames:
+#  dry run:
+detox -r -v -n <path to folder or file>
+
+#  normal run:
+detix -r -v <path to folder or file>
+
+#  postprocessing to remove some characters not captured by detox:
+files=`find <my_folder> -name "*+*"`
+for f in $files
+do
+  mv "${f}" $(echo "${f}" | sed -e 's/[+]/et/g')
+done
+
+files=`find <my_folder> -name "*%*"`
+for f in $files
+do
+  mv "${f}" $(echo "${f}" | sed -e 's/[%]/_/g')
+done
+
+files=`find <my_folder> -name "*~*"`
+for f in $files
+do
+  mv "${f}" $(echo "${f}" | sed -e 's/[~]/_/g')
+done
+
+files=`find <my_folder> -name "*,*"`
+for f in $files
+do
+  mv "${f}" $(echo "${f}" | sed -e 's/[,]//g')
+done
+
